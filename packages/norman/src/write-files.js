77eb7197230994101b76792ac29504cc14c87b24
Kennedy Rose <kennedyianrose@gmail.com>
2020-10-26 17:11:37 -0500

    max depth

--- packages/norman/src/write-files.js
+++ packages/norman/src/write-files.js
@@ -1,26 +1,50 @@
 const { outputFile } = require(`fs-extra`)
 const { join } = require(`path`)
 const download = require(`download`)
-const js = require(`javascript-stringify`)
+// const js = require(`javascript-stringify`)
 const toml = require(`@iarna/toml`)
 
 const cwd = process.cwd()
 
-function removeCircularReferences(o){
-	const cache = []
-	o = JSON.stringify(o, function(_, value) {
-		if (typeof value === `object` && value !== null) {
-			if (cache.indexOf(value) !== -1) {
-				// Circular reference found, discard key
-				return
-			}
-			// Store value in our collection
-			cache.push(value)
+const maxDepth = 200
+
+function deepClone(o, depth = 0){
+	depth = depth + 1
+	if(typeof o !== `object`){
+		return o
+	}
+	if(depth > maxDepth){
+		return
+	}
+	if(Array.isArray(o)){
+		const clone = []
+		for(let i = 0; i < o.length; i++){
+			clone[i] = deepClone(o[i], depth)
 		}
-		return value
-	})
-	return JSON.parse(o)
+		return clone
+	}
+	const clone = {}
+	for(let i in o){
+		clone[i] = deepClone(o[i], depth)
+	}
+	return clone
 }
+
+// function removeCircularReferences(o){
+// 	const cache = []
+// 	o = JSON.stringify(o, function(_, value) {
+// 		if (typeof value === `object` && value !== null) {
+// 			if (cache.indexOf(value) !== -1) {
+// 				// Circular reference found, discard key
+// 				return
+// 			}
+// 			// Store value in our collection
+// 			cache.push(value)
+// 		}
+// 		return value
+// 	})
+// 	return JSON.parse(o)
+// }
 
 module.exports = async function writeFiles() {
 	console.log(`Writing files...`)
@@ -34,10 +58,11 @@ module.exports = async function writeFiles() {
 		let { space } = options
 		if (!space) space = null
 		else if (space == true) space = 3
+		space = 3
 		let contents = this.files[originalPath]
 
 		// Remove circular references
-		contents = removeCircularReferences(contents)
+		contents = deepClone(contents)
 
 		let path = join(cwd, options.dist, `${originalPath}.${options.filetype}`)
 		if (options.filetype === `json`) {
@@ -48,7 +73,8 @@ module.exports = async function writeFiles() {
 			contents = JSON.stringify(contents)
 			contents = JSON.parse(contents)
 
-			contents = js.stringify(contents, null, space)
+			// contents = js.stringify(contents, null, space)
+			contents = JSON.stringify(contents, null, space)
 			contents = [`module.exports`, `=`, contents].join(space ? ` ` : ``)
 		}
 		else if (options.filetype === `toml`) {
